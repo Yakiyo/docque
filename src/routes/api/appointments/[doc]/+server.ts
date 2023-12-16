1f9c1ce4707e62e41519e0557a93d19958186e60
Yakiyo <yakiyo1206@gmail.com>
2023-12-16 16:18:58 +0000

    feat: create appointment endpoint

--- src/routes/api/appointments/[doc]/+server.ts
+++ src/routes/api/appointments/[doc]/+server.ts
@@ -27,3 +27,48 @@ export async function GET({ params: { doc } }) {
 		error: null
 	});
 }
+
+/**
+ * Adds a new appointment for a doctor
+ */
+export async function POST({ params: { doc }, request }) {
+	// NOTE: duration is and MUST be in minutes
+	const { start, duration, patient } = (await request.json()) as Record<string, string>;
+	if ([start, duration, patient].some((x) => !x)) {
+		return json({
+			ok: false,
+			data: null,
+			error: 'Missing required property. Must provide start, duration and patient'
+		});
+	}
+
+	const doctor = await prisma.doctor.findUnique({
+		where: {
+			name: doc
+		},
+		select: {
+			id: true,
+		}
+	});
+	if (!doctor) {
+		return json({
+			ok: false,
+			data: null,
+			error: `Invalid doctor name. No entry found for ${doc}`
+		});
+	}
+	const err = await prisma.appointment.create({
+		data: {
+			start: new Date(start),
+			end: new Date(new Date(start).getTime() + (Number(duration) * 60000)),
+			patient,
+			doctorId: doctor.id,
+		}
+	}).then(() => null).catch(e => `${e}`);
+	
+	return json({
+		ok: !err ? true : false,
+		data: null,
+		error: err,
+	})
+}
