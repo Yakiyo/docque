df58d4ab979090beb6717fa9f526644d46f75d1a
Yakiyo <yakiyo1206@gmail.com>
2023-12-17 15:25:16 +0000

    feat: load function

---
+++ src/routes/+page.ts
@@ -0,0 +1,13 @@
+import type { ApiResponse, AppointmentResponse } from '$lib';
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch }) => {
+    const response = await fetch('/api/appointments').then(r => r.json()) as ApiResponse<AppointmentResponse[]>;
+    if (!response.ok) {
+        error(400, response.error!);
+    }
+    return {
+        appointments: response.data,
+    }
+}
