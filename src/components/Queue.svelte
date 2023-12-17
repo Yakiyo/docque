255a7b5f5f82d248c9f2ff26a0210be68fc3048a
Yakiyo <yakiyo1206@gmail.com>
2023-12-17 15:58:27 +0000

    feat: queue component

---
+++ src/components/Queue.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { AppointmentResponse } from '$lib';
+	export let doctor: string;
+	export let appointments: AppointmentResponse[];
+	appointments.sort((a, b) => a.start.getTime() - b.start.getTime());
+</script>
+
+<table border=5>
+	<caption>Appointments for Dr. {doctor}</caption>
+	<tr>
+		<th>Serial</th>
+		<th>Patient</th>
+		<th>Estimated Time</th>
+		<th>Estimated Duration</th>
+	</tr>
+	{#if appointments.length < 1}
+		<tr>
+			<td colspan="4">Dr. {doctor} currently has no appointments</td>
+		</tr>
+	{/if}
+	{#each appointments as appointment, i}
+		<tr>
+			<td>{i + 1}</td>
+			<td>{appointment.patient}</td>
+			<td>{appointment.start}</td>
+			<td>{appointment.duration}</td>
+		</tr>
+	{/each}
+</table>
