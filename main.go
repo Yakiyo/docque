e8f0fd7c6d96b449d6074625305c5930d3948b64
Yakiyo <yakiyo1206@gmail.com>
2024-01-18 06:31:18 +0000

    feat: setup gin

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/Yakiyo/docque/api"
+	"github.com/charmbracelet/log"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello")
+	port := "8080"
+	if val := os.Getenv("PORT"); val != "" {
+		port = val
+	}
+	// log stuff to stderr, not stdout
+	gin.DefaultWriter = os.Stderr
+	
+	app := gin.Default()
+	
+	// serve all files in /public
+	app.Static("/", "./public")
+
+	router := app.Group("/api")
+
+	api.Register(router)
+
+	log.Info("Starting server", "port", port)
+	err := app.Run(":" + port)
+	if err != nil {
+		log.Fatal("Error when starting server", "err", err)
+	}
 }
