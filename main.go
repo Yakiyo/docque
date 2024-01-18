8c9219f929d32d6ec5382686d711665c4f6207cf
Yakiyo <yakiyo1206@gmail.com>
2024-01-18 09:18:03 +0000

    chore: open and close db

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/Yakiyo/docque/api"
+	"github.com/Yakiyo/docque/db"
 	"github.com/charmbracelet/log"
 	"github.com/gin-gonic/gin"
 )
@@ -13,11 +14,15 @@ func main() {
 	if val := os.Getenv("PORT"); val != "" {
 		port = val
 	}
+
+	db.Init()
+	defer db.Close()
+
 	// log stuff to stderr, not stdout
 	gin.DefaultWriter = os.Stderr
-	
+
 	app := gin.Default()
-	
+
 	// serve all files in /public
 	app.Static("/", "./public")
 
